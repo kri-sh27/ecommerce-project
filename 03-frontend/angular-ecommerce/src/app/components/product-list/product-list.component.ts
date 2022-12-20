5c89d0286937c6ab1d4efed4a64635ce58bcffe2
kri-sh27 <hogalekrishna2000@gmail.com>
2022-12-20 15:30:39 +0530

    pagination

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -14,7 +14,14 @@ export class ProductListComponent {
 
   products:Product[]=[];
   currentCategoryId:number=1;
+  previousCategoryId:number=1;
   searchMode:boolean=false;
+
+  //new properties forpagination
+thePageNumber:number=1;
+thePageSize:number=10;
+theTotalElements:number=0;
+
   constructor(private productService:ProductService,
               private route:ActivatedRoute){
 
@@ -62,13 +69,30 @@ export class ProductListComponent {
       this.currentCategoryId=1;
     }
 
+    //check if we have a differnet catefory than previous
+    //Note:Angular will reuse a component if it is currently being viewed
+    // if we have a different category id than previous 
+    //than set thePageNumber back to 1
+
+    if (this.previousCategoryId!=this.currentCategoryId){
+      this.thePageNumber=1;
+    }
+
+    this.previousCategoryId=this.currentCategoryId;
+     
+    console.log(`currentCategoryId=${this.currentCategoryId},thePageNumber=${this.thePageNumber}`);
+
+
 
     //now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
+    this.productService.getProductListPaginate(this.thePageNumber-1,this.thePageSize,this.currentCategoryId).subscribe(
       data=>{
-        this.products=data;
+        this.products=data._embedded.products;
+        this.thePageNumber=data.page.number+1;
+        this.thePageSize=data.page.size;
+        this.theTotalElements=data.page.totalElements;
       }
-    )
+    );
 
   }
 
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -10,12 +10,6 @@ import { ProductCategory } from '../common/product-category';
 export class ProductService {
 
 
-
-
-
-
-
-
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
@@ -28,6 +22,15 @@ export class ProductService {
     const productUrl = `${this.baseUrl}/${theProductId}`;
     return this.httpClient.get<Product>(productUrl);
   }
+
+  getProductListPaginate(thePage: number, thePageSize: number, theCategoryId: number): Observable<GetResponseProducts> {
+    // need to build URl based on category id , page and size
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`+`&page=${thePage}&size=${thePageSize}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+
+  }
+
+
   getProductList(theCategoryId: number): Observable<Product[]> {
     // need to build URl based on category id 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
@@ -56,7 +59,17 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
+
+
   }
+
+
 }
 
 interface GetResponseProductCategory {
